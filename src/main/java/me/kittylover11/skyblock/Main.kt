7a5c6e700bd499081d4e1ebd3634c0b88a8cff86
kittylover11 <kittylover11@kittylover11.kittylover11>
2024-03-17 21:15:42 -0500

    Init

---
+++ src/main/java/me/kittylover11/skyblock/Main.kt
@@ -0,0 +1,13 @@
+package me.kittylover11.skyblock
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    override fun onEnable() {
+        println("hello")
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
