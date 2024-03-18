71ae19ec49b889c2a8859ea5d98ae5d7fc9ede2c
kittylover11 <kittylover11@kittylover11.kittylover11>
2024-03-17 21:43:15 -0500

    Changed from paper to spigot

--- src/main/java/me/kittylover11/skyblock/Main.kt
+++ src/main/java/me/kittylover11/skyblock/Main.kt
@@ -1,13 +1,21 @@
 package me.kittylover11.skyblock
 
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerChatEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-class Main : JavaPlugin() {
+class Main : JavaPlugin(), Listener {
     override fun onEnable() {
         println("hello")
+        server.pluginManager.registerEvents(this, this)
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    fun onChat(e: PlayerChatEvent) {
+        println("This event works")
     }
 }
